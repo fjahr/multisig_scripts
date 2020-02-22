d7786d905ecf652024cd092868af8ca47f0d075a
Hugo Nguyen <hugh.hn@gmail.com>
2020-02-22 05:58:17 -0800

    comments for finish.py

--- finish.py
+++ finish.py
@@ -2,6 +2,18 @@
 
 import argparse
 
+# 1/ Combine PSBTs:
+#   ./bitcoin-cli [-testnet|-mainnet] -datadir=[DATADIR] -rpcwallet=[WALLET] combinepsbt [PSBT1, PSBT2]
+
+# 2/ Finalize PSBT:
+#   ./bitcoin-cli [-testnet|-mainnet] -datadir=[DATADIR] -rpcwallet=[WALLET] finalizepsbt [PSBT]
+
+# 3/ Decode raw transaction, show addresses and amounts one more time
+#   ./bitcoin-cli [-testnet|-mainnet] -datadir=[DATADIR] -rpcwallet=[WALLET] decoderawtransaction [RAW_TX]
+
+# 4/ Send raw transaction
+#   ./bitcoin-cli [-testnet|-mainnet] -datadir=[DATADIR] -rpcwallet=[WALLET] sendrawtransaction [RAW_TX]
+
 def finish():
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', '--test', type=str, default="foo")
